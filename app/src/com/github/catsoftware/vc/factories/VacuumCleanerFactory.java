179c7fe3a69a22e6dfef0f0406b48899e809b93f
Rod1Andrade <rodrigopires.profissional@gmail.com>
2021-12-13 10:07:33 -0300

    UPDATE - Vacuum cleaner instance by vacuum cleaner model

---
+++ app/src/com/github/catsoftware/vc/factories/VacuumCleanerFactory.java
@@ -0,0 +1,10 @@
+package com.github.catsoftware.vc.factories;
+
+import com.github.catsoftware.vc.entities.VacuumCleanerRenderEntity;
+import com.github.catsoftware.vc.models.VacuumCleanerModel;
+
+public class VacuumCleanerFactory {
+    public static VacuumCleanerRenderEntity factoryEntityBy(VacuumCleanerModel vacuumCleanerModel) {
+        return new VacuumCleanerRenderEntity(0, 0, 16, 16, vacuumCleanerModel.getDirection());
+    }
+}
